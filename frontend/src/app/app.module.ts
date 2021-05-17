d3f4b56bc82c607caad9ad8785f0397cf09d05dc
Marcos Renan Krul <marcoskrul2k@gmail.com>
2021-05-17 13:50:58 -0300

    estrutural inicial p/ componente de criacao

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,9 +11,11 @@ import { NavComponent } from './components/template/nav/nav.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
+import { ProductCreateComponent } from './components/products/product-create/product-create.component';
 
 @NgModule({
   declarations: [
@@ -23,12 +25,14 @@ import { RedDirective } from './directives/red.directive';
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    RedDirective
+    RedDirective,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
     MatListModule,
     MatCardModule,
+    MatButtonModule,
     MatToolbarModule,
     MatSidenavModule,
     AppRoutingModule,
